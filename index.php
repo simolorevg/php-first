04bb6a4115c1d4e80b7cdc644259073b976cdc57
simolorevg <simoneviaggi@libero.it>
2023-05-03 11:44:57 +0200

    First php: shock!

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$username = 'Simone';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primo PHP</title>
+</head>
+
+<body>
+    <h1>Hello World From PHP</h1>
+    <h2> Ciao <?php echo $username; ?></h2>
+</body>
+
+</html>
